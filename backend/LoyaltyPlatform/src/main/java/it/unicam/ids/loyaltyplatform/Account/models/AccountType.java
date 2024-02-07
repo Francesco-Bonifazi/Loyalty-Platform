76d4dfea3d164a1dd032c0dcaf96f1ffdc83d4a1
Francesco Bonifazi <francesco.bonifazi@studenti.unicam.it>
2024-02-07 19:44:42 +0100

    [FIX] Errore caricamento
    Errore durante il caricamento del backend finale

--- backend/LoyaltyPlatform/src/main/java/it/unicam/ids/loyaltyplatform/Account/models/AccountType.java
+++
@@ -1,5 +0,0 @@
-package it.unicam.ids.loyaltyplatform.account.models;
-
-public enum AccountType {
-    UTENTE, CASSIERE, TITOLARE, AMMINISTRATORE
-}
--- backend/LoyaltyPlatform/src/main/java/it/unicam/ids/loyaltyplatform/PF/models/PFType.java
+++
@@ -1,6 +0,0 @@
-package it.unicam.ids.loyaltyplatform.pf.models;
-
-public enum PFType {
-    PFPUNTI, PFLIVELLI, PFCASHBACK, PFVIP, PFCOALIZIONE
-}
-
